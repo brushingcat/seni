887aadddfd101885006b108aad6d05dbb7ef49e6
brushingcat <antonio.gf.pinheiro@hotmail.com>
2019-02-19 16:38:54 +0000

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -25,71 +25,87 @@
 $(document).ready(function() {
     var section_actual = "0";
     var target = "";
+    var scroll_flag = false;
     $(window).bind('mousewheel', function(e){
-        if(e.originalEvent.wheelDelta > 0)
+        event.preventDefault();
+        if(!scroll_flag)
         {
-            if(section_actual=="sec1")
+            scroll_flag=true;
+            if(e.originalEvent.wheelDelta > 0)
             {
-                target = $("#sec0");
-                section_actual = "sec0";
-            }else{
-                var number_section = section_actual.substring(3,4);
-                console.log("first part");
-                console.log(section_actual.substring(0,3));
-                console.log("second part");
-                console.log(number_section);
-                section_actual = section_actual.substring(0,3)+(parseInt(number_section, 10)-1);
-                console.log(section_actual);
-            }
-           
-            if(parseInt(number_section)<1)
-            {
-                section_actual="sec0";
-            } else {
-                target = $("#"+section_actual);
-                if( target.length ) {
-                    
-                    $('html, body').stop().animate({
-                        scrollTop: target.offset().top
-                    }, 700);
-                    event.preventDefault();
+                if(section_actual=="sec1")
+                {
+                    target = $("#sec0");
+                    section_actual = "sec0";
+                }else{
+                    var number_section = section_actual.substring(3,4);
+                    console.log("first part");
+                    console.log(section_actual.substring(0,3));
+                    console.log("second part");
+                    console.log(number_section);
+                    section_actual = section_actual.substring(0,3)+(parseInt(number_section, 10)-1);
+                    console.log(section_actual);
+                }
+            
+                if(parseInt(number_section)<1)
+                {
+                    section_actual="sec0";
+                } else {
+                    target = $("#"+section_actual);
+                    if( target.length ) {
+                        
+                        $('html, body').stop().animate({
+                            scrollTop: target.offset().top
+                        }, 700);
+                        event.preventDefault();
+                    }
                 }
             }
-        }
-        else
-        {
-            console.log("down");
-
-            if(section_actual=="0")
+            else
             {
-                target = $("#sec1");
-                section_actual = "sec1";
-            }else{
-                var number_section = section_actual.substring(3,4);
-                console.log("first part");
-                console.log(section_actual.substring(0,3));
-                console.log("second part");
-                console.log(number_section);
-                section_actual = section_actual.substring(0,3)+(parseInt(number_section, 10)+1);
-                console.log(section_actual);
-            }
+                console.log("down");
 
-            if(parseInt(number_section)>3)
-            {
-                section_actual="sec3";
-            } else {
-                target = $("#"+section_actual);
-                if( target.length ) {
-                    
-                    $('html, body').stop().animate({
-                        scrollTop: target.offset().top
-                    }, 700);
-                    event.preventDefault();
+                if(section_actual=="0")
+                {
+                    target = $("#sec1");
+                    section_actual = "sec1";
+                }else{
+                    var number_section = section_actual.substring(3,4);
+                    console.log("first part");
+                    console.log(section_actual.substring(0,3));
+                    console.log("second part");
+                    console.log(number_section);
+                    section_actual = section_actual.substring(0,3)+(parseInt(number_section, 10)+1);
+                    console.log(section_actual);
                 }
+
+                if(parseInt(number_section)>3)
+                {
+                    section_actual="sec3";
+                } else {
+                    target = $("#"+section_actual);
+                    if( target.length ) {
+                        
+                        $('html, body').stop().animate({
+                            scrollTop: target.offset().top
+                        }, 700);
+                        console.log("ola");
+                        event.preventDefault();
+                    }
+                }
+
+                
             }
+            setTimeout(function () {
+                scroll_flag=false;
+                console.log("flag=false");
+            }, 1000);
 
             
+        } else {
+            console.log("não entrou flag era true");
         }
+        
     });
 
 
